9fbb92a676ab1ccefed03597f990ebe51a4659cc
Mariana <stasiakmariana@gmail.com>
2022-06-28 09:03:14 +0100

    updated packages, added tailwind and removed styled-components

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 A template for a Next.js / Typescript project with styled-component and atomic design principles
+# incard-project
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,14 +1,9 @@
-import { Home } from "components/templates";
-import { HomeWrapper } from "./home.styles";
-
 interface HomePageProps {}
 
 const HomePage: React.FC<HomePageProps> = ({}) => {
   return (
     <>
-      <HomeWrapper>
-        <Home />
-      </HomeWrapper>
+      <h1>hello</h1>
     </>
   );
 };
